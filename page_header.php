6cbb7c23dea25b19293f6cc7e62c7cdb2e0ef0f5
root <root@deep-net.org>
2012-12-13 06:37:21 -0600

    fixed links

--- page_header.php
+++ page_header.php
@@ -11,13 +11,13 @@
     <div align="center">
       <div style="background-color:#333; color:#fff; height:55px;"><br />
         <span style="color:#ccc; font-size:16px;">
-          <a href="http://askin.ws/tools" style="color:#ccc;">~/tools</a>
+          <a href="/" style="color:#ccc;">~/tools</a>
           &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
         </span>
         <a href="#" class="fdg" style="color:#fff; font-size:26px; text-decoration:none;">kelebek's tools</a>
         <span style="color:#ccc; font-size:16px;">
           &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-          <a href="http://askin.ws" style="color:#ccc;">www.askin.ws</a>
+          <a href="http://askin.ws" style="color:#ccc;">~/askin</a>
         </span>
       </div>
       <br /><br />
