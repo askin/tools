03c96b7cc71ea7426bd911f0ba7610b4d50378a4
Aşkın Yollu <askin@askin.ws>
2013-11-04 14:22:12 +0100

    fix text areas size

--- get-header.php
+++ get-header.php
@@ -4,10 +4,10 @@ include('page_header.php');
 ?>
         <table width="800" cellspacing="0" cellpadding="0" border="0">
 	  <tr>
-            <td width="50%" style="padding:30px; vertical-align:top; padding-top:0px;">
+            <td style="padding:30px; vertical-align:top; padding-top:0px;">
               <form onsubmit="return bomb(this);" name="myform" action="">
                 <h1 style="font-size:30px;"><a href="?" style="text-decoration:none; color:#C00;">enter url</a></h1>
-                <input type="text" name="gelen" id="gelen" style="font-size:19px; width:350px; border:18px #C00 solid" />
+                <input type="text" name="gelen" id="gelen" style="font-size:19px; width:700px; border:18px #C00 solid" />
                 <br />
                 <br />
                 <input type="submit" name="button" id="button" value="get header" style="font-size:19px;" />
@@ -18,4 +18,3 @@ include('page_header.php');
         <div id="sonuc" style="color:#666; font-size:16px; text-align:left; width: 750px;"></div>
         <br />
  <?php include('footer.php'); ?>
-
--- whois.php
+++ whois.php
@@ -4,10 +4,10 @@ include('page_header.php');
 ?>
         <table width="800" cellspacing="0" cellpadding="0" border="0">
 	  <tr>
-            <td width="50%" style="padding:30px; vertical-align:top; padding-top:0px;">
+            <td style="padding:30px; vertical-align:top; padding-top:0px;">
               <form onsubmit="return whois(this);" name="myform" action="">
                 <h1 style="font-size:30px;"><a href="?" style="text-decoration:none; color:#C00;">enter domain</a></h1>
-                <input type="text" name="domain" id="domain" style="font-size:19px; width:350px; border:18px #C00 solid" />
+                <input type="text" name="domain" id="domain" style="font-size:19px; width:700px; border:18px #C00 solid" />
                 <br />
                 <br />
                 <input type="submit" name="button" id="button" value="who is?" style="font-size:19px;" />
